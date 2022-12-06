bb26772f6c1e10377bb865a5b43a4f88d16afbbb
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-12-06 15:20:46 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
+/*
+Modifichiamo il codice dell'esercizio per renderlo funzionante con
+un array di oggetti, con una regola: non potete modificare l'array di oggetti in nessun caso.
+Dato un array di oggetti letterali con:
+   url dell’immagine
+   titolo
+   descrizione
+Creare un carosello.
+
+Milestone 0:
+Popoliamo dinamicamente il contenuto del carosello con i dati forniti dall'array di oggetti,
+dal js (dentro al nostro 'carousel-item', per capirci).
+
+Milestone 1:
+Al click dell'utente sulle frecce verso l'alto o verso il basso,
+l'immagine attiva diventerà visibile.
+
+Milestone 2:
+Aggiungiamo alla visualizzazione delle immagini anche titolo e testo relative alla singola immagine.
+Bonus 1:
+
+Aggiungere il ciclo infinito del carosello.
+Ovvero se l'immagine attiva è la prima e l'utente clicca la freccia verso l'alto,
+l'immagine che deve attivarsi sarà l'ultima e
+viceversa per l'ultima immagine se l'utente clicca la freccia verso il basso.
+Bonus 2:
+
+Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.
+Bonus 3:
+
+Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva
+dovrà cambiare alla successiva.
+*/
+
+
+
 const images = [
    {
       image: 'img/01.webp',
@@ -25,3 +61,45 @@ const images = [
       text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
    }
 ];
+
+
+
+
+let imageContainer = document.querySelector('.carousel-image');
+
+let previousButton = document.querySelector('.button.previous');
+console.log(previousButton);
+
+let nextButton = document.querySelector('.button.next');
+console.log(nextButton);
+
+let image
+
+images.forEach((element) => {
+   image = document.createElement('img');
+   console.log(element.image);
+   image.setAttribute('src', `./${element.image}`);
+   image.classList.add('image', 'none');
+   imageContainer.append(image);
+   element.none = "none";
+});
+console.log(images)
+
+
+nextButton.addEventListener ('click', function(){
+
+   images.forEach((element) => {
+      
+   });
+   image.classList.remove('none');
+
+
+   
+   
+})
+
+
+
+   
+
+
